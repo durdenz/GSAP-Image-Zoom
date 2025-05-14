d9fc582ebaecebe4d21c5b9f3e5ba3fbda483867
George Durden <george@durden.com>
2025-05-14 11:31:05 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline({
+ease: "none"
+});
+
+tl.from("#image img", {
+  scale: .01,
+  duration: 1,
+  transformOrigin: "center center",
+}).to({}, {
+  duration: 1
+})
+ScrollTrigger.create({
+  trigger: "#image",
+  start: "top top",
+  end: "+=100%",
+  pin: true,
+  animation: tl,
+  scrub: 0.78,
+  pinSpacing: false
+})
